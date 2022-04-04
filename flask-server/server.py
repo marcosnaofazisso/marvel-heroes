a49ee25f1ecb60327fb33f817e244b63aa1b0a73
Marcos Ferreira <marcosferreiraf22@gmail.com>
2022-04-04 18:17:54 -0300

    add requests module and now server is working fine

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,48 +1,37 @@
-# import requests
-from flask import Flask, render_template
-from flask import request
-import json
+import requests
+try:
+    from flask import Flask, jsonify, request
+    from flask import request
+    import json
+except ImportError as ie:
+    print("Error importing required packages: {ie}")
 
-timestamp = "1649095132"
+
+ts = "1649095132"
 apikey = "e423b333a7aba87d5f832698af6bc1eb"
 hashkey = "9f97209a76d8959d9497419381a93284"
-base = "https://gateway.marvel.com"
-requisicao = "/v1/public/characters?"
+base = "https://gateway.marvel.com:443"
+req = "/v1/public/characters?"
 
-URL = base + requisicao + "ts=" + timestamp + "&apikey=" + apikey + "&hash=" + hashkey
+URL = base + req + "ts=" + ts + "&apikey=" + apikey + "&hash=" + hashkey
 
 app = Flask(__name__)
 
 
-
-@app.route("/teste", methods=['GET'])
+@app.route("/heroes", methods=["GET"])
 def index():
-    return ("XXXXXXXX")
+    resp = requests.get(URL).json()
+    try: 
+        name = resp["data"]["results"][0]["name"]
+        description = resp["data"]["results"][0]["description"]
+        thumbnail = resp["data"]["results"][0]["thumbnail"]["path"]
+        all_heroes = dict()
+        for index, value in enumerate(resp["data"]["results"]):
+            all_heroes[index] = value
+        return(all_heroes)
+    except:
+        return("ERROR: Thanos won this time :( ")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-# timestamp = "1649095132"
-# apikey = "e423b333a7aba87d5f832698af6bc1eb"
-# hashkey = "9f97209a76d8959d9497419381a93284"
-# base = "https://gateway.marvel.com"
-# requisicao = "/v1/public/characters?"
-
-# URL = base + requisicao + "ts=" + timestamp + "&apikey=" + apikey + "&hash=" + hashkey
-
-# dados = requests.get(URL).json()
-# try:
-#     todos_nomes = dados["data"]["results"]
-# except:
-#     exit("Vamos testar isso!")
-
-
-# nome = dados["data"]["results"][0]["name"]
-
-
-
