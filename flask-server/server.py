9224f6d9cf0ba9f9844fd811d0be511acc78e0de
Marcos Ferreira <marcosferreiraf22@gmail.com>
2022-04-04 22:13:15 -0300

    changing obj structures... need to check

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,29 +6,30 @@ try:
 except ImportError as ie:
     print("Error importing required packages: {ie}")
 
-
+limit = "55"
 ts = "1649095132"
 apikey = "e423b333a7aba87d5f832698af6bc1eb"
 hashkey = "9f97209a76d8959d9497419381a93284"
 base = "https://gateway.marvel.com:443"
 req = "/v1/public/characters?"
 
-URL = base + req + "ts=" + ts + "&apikey=" + apikey + "&hash=" + hashkey
+URL = base + req + "limit=" + limit + "&ts=" + ts + "&apikey=" + apikey + "&hash=" + hashkey
 
 app = Flask(__name__)
 
 
-@app.route("/heroes", methods=["GET"])
+@app.route("/rest", methods=["GET"])
 def index():
     resp = requests.get(URL).json()
+    pack = {}
+    lista = []
     try: 
-        name = resp["data"]["results"][0]["name"]
-        description = resp["data"]["results"][0]["description"]
-        thumbnail = resp["data"]["results"][0]["thumbnail"]["path"]
-        all_heroes = dict()
-        for index, value in enumerate(resp["data"]["results"]):
-            all_heroes[index] = value
-        return(all_heroes)
+        for i in range(0, len(resp["data"]["results"])):
+            name = resp["data"]["results"][i]["name"]
+            description = resp["data"]["results"][i]["description"]
+            thumbnail = resp["data"]["results"][i]["thumbnail"]["path"] + ".jpg"
+            pack[i] = {"name": name,"description": description, "thumbnail": thumbnail }
+        return pack
     except:
         return("ERROR: Thanos won this time :( ")
 
--- marvel/src/App.js
+++ marvel/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const [heroes, setHeroes] = useState([])
+
+  useEffect(() => {
+    fetch("/rest").then(resp =>
+      resp.json().then(data => {
+        console.log("DATA" + JSON.stringify(data[0]))
+        setHeroes(data);
+        console.log("XXXXXXXXXXXXXXXXXXXXXX" + JSON.stringify(heroes))
+      }))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{heroes[0].name}</h1>
     </div>
-  );
+  )
 }
 
 export default App;
+
+{/* <div key={heroe.data.offset}> */ }
+{/* <p>{heroes["data"]["results"][0]["name"]}</p> */ }
+{/* <p>{JSON.stringify(heroes["data"]["offset"])}</p> */ }
+
+{/* <div>
+      <h1>{heroes[0].name}</h1>
+      <p>{heroes[0].description}</p>
+      <img src={heroes[0].thumbnail} alt={heroes[0].name} />
+      </div> */}
